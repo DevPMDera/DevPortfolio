f0e26da37df8e7469b441a21d4cfd59059a9e864
DevPMDera <deraiphone2025@gmail.com>
2026-04-09 18:46:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ tabButtons.forEach(button => {
     document.getElementById(button.dataset.tab).classList.add('active');
   });
 });
+
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = themeToggle.querySelector('.material-symbols-outlined');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    themeIcon.textContent = 'light_mode';
+  } else {
+    themeIcon.textContent = 'dark_mode';
+  }
+});
